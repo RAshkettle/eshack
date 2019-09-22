3f36c93f38261ca1bdf19ce06ec3f98fe4809da1
Richard Ashkettle <richard.ashkettle@gmail.com>
2019-09-22 14:55:42 -0400

    added default settings for map

--- src/config.js
+++ src/config.js
@@ -5,4 +5,9 @@ export default {
     parent: 'phaser-game',
     width: 1024,
     height: 768,
+    MAP_WIDTH: 62,
+    MAP_HEIGHT: 45,
+    ROOM_MAX_SIZE: 10,
+    ROOM_MIN_SIZE: 6,
+    MAX_ROOMS: 30,
 };
